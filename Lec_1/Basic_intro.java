1d5b5ec86cfcaa30974eae6f40866b2191025d3b
harshfi <harshcooltripathi1@gmail.com>
2024-09-03 22:17:51 +0530

    Basic understanding of programming

---
+++ Lec_1/Basic_intro.java
@@ -0,0 +1,20 @@
+package Lec_1;
+
+public class Basic_intro {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=10;
+	    int j=12;
+	    int sum=i+++j---i---6+i;
+	    System.out.println(sum);
+		
+		
+	}
+     public static int fun() {
+    	 System.out.println("Im in fun");
+    	 return 6;
+     }
+}
