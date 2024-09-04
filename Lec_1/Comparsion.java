63b6761092bb5fa9ea3f76aabec53c9ea5617a15
harshfi <harshcooltripathi1@gmail.com>
2024-09-04 22:27:07 +0530

    Basic UnderStanding Of Programming

---
+++ Lec_1/Comparsion.java
@@ -0,0 +1,17 @@
+package Lec_1;
+
+public class Comparsion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int y = 7;
+		int x = 5;
+		System.out.println(x == y);
+		System.out.println(x != y);
+
+		System.out.println(x > y);
+		System.out.println(x < y);
+
+	}
+
+}
